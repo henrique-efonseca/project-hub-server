596254a8e792369f463413484dd45a9a21a84881
Henrique Fonseca <henrique@fonseca.email>
2024-07-12 17:19:22 +0100

    Update projectsConfigs.ts

--- scripts/projectsConfigs.ts
+++ scripts/projectsConfigs.ts
@@ -1,14 +1,14 @@
-export const PROJECTS_CONFIGS = [
+[
   {
-    project: "henriquefonseca.me",
-    repository: "https://github.com/henrique-efonseca/henriquefonseca.me",
-    localPath: "../../henriquefonseca.me/.env.local",
-    remotePath: "/home/ubuntu/henriquefonseca.me/.env.local",
+    "project": "henriquefonseca.me",
+    "repository": "https://github.com/henrique-efonseca/henriquefonseca.me",
+    "localPath": "../../henriquefonseca.me/.env.local",
+    "remotePath": "/home/ubuntu/henriquefonseca.me/.env.local"
   },
   {
-    name: "reverse-proxy",
-    repository: "https://github.com/henrique-efonseca/project-hub-server",
-    localPath: "../reverse-proxy/certs",
-    remotePath: "/home/ubuntu/project-hub-server/reverse-proxy",
-  },
-];
+    "name": "reverse-proxy",
+    "repository": "https://github.com/henrique-efonseca/project-hub-server",
+    "localPath": "../reverse-proxy/certs",
+    "remotePath": "/home/ubuntu/project-hub-server/reverse-proxy"
+  }
+]
