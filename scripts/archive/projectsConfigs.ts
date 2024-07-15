0b2e7a0226e811c96aa0fa05deaf28806376f0d3
Henrique Fonseca <henrique@fonseca.email>
2024-07-15 23:06:16 +0100

    Create projectsConfigs.ts

---
+++ scripts/archive/projectsConfigs.ts
@@ -0,0 +1,16 @@
+[
+  {
+    "project": "henriquefonseca.me",
+    "repository": "https://github.com/henrique-efonseca/henriquefonseca.me",
+    "localPath": "../../henriquefonseca.me/.env.local",
+    "remotePath": "/home/ubuntu/henriquefonseca.me/.env.local",
+    "remoteDockerPath": "/home/ubuntu/henriquefonseca.me/docker"
+  },
+  {
+    "name": "reverse-proxy",
+    "repository": "https://github.com/henrique-efonseca/project-hub-server",
+    "localPath": "../reverse-proxy/certs",
+    "remotePath": "/home/ubuntu/project-hub-server/reverse-proxy",
+    "remoteDockerPath": "/home/ubuntu/project-hub-server/reverse-proxy/docker"
+  }
+]
